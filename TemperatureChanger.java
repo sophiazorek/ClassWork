a5922e461efaf0edfabf5ee680f6588a40194bc2
sophiazorek <sophia_zorek@asl.org>
2014-09-30 13:42:45 +0100

    Update TemperatureChanger.java

--- TemperatureChanger.java
+++ TemperatureChanger.java
@@ -5,16 +5,29 @@
 *  @version 25 September, 2014
 **/
 
-import java.util.Scanner;
-
-public class TemperatureChanger {
+ import java.util.*;
+ 
+ public class TemperatureChanger {
 
   public static void main (String[] args) {
-    // TODO - scan for input in the format 175F or 18C 
-    // TODO - perform correct conversion
-    // TODO - print out results to the screen
+        Scanner temperatureInput = new Scanner(System.in);
+        System.out.println("Please enter a Temperature and indicate if it is in Farenheit or Celcius with an F or a C!");
+        String numberInput = temperatureInput.nextLine();
+        
+        if (numberInput.charAt(numberInput.length()-1) == 'F') {
+            numberInput = numberInput.substring(0,numberInput.length() - 1);
+            double result = Double.parseDouble(numberInput);
+        System.out.println(((result * (5/9)) - 32));
+    
+   
+   }
+        if (numberInput.charAt(numberInput.length()-1) == 'C') {
+            numberInput = numberInput.substring(0,numberInput.length() -1);
+            double result = Double.parseDouble(numberInput);
+            System.out.println((result*(9/5) + 32));
   }
-  
+}
+
 }
 
 
